e5c00e698a818b998c42719b634a05d11a20df9c
dantleech <dan.t.leech@gmail.com>
2016-04-20 16:08:58 +0100

    Doctinre PHPCR-ODM non-mandatory

--- lib/DependencyInjection/CmfAdminExtension.php
+++ lib/DependencyInjection/CmfAdminExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Doctrine\ODM\PHPCR\DocumentManagerInterface;
 
 class CmfAdminExtension extends Extension
 {
@@ -13,6 +14,9 @@ class CmfAdminExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $loader->load('form.xml');
+        // TODO: Handle this in a better way? (or make it an explicit config option).
+        if (class_exists(DocumentManagerInterface::class)) {
+            $loader->load('doctrine-phpcr-odm/form.xml');
+        }
     }
 }
